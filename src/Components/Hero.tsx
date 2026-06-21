fc2f5a15b77b642d78855fcef6aefbec8b7588bf
Leon Gustavo <leongtg88@gmail.com>
2026-06-20 23:21:06 -0400

    banner hero de desktop practicamente listo, pasando a mobile

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -18,133 +18,157 @@ import heroImageMovilpeque from '../assets/Movil peque.svg';
 const Hero = () => {
   return (
     <div
-      className="relative h-screen lg:h-[1000px] w-full bg-no-repeat bg-cover bg-center bg-fixed"
+      className="relative h-[1000px] lg:h-[800px] w-full bg-no-repeat bg-cover bg-center bg-fixed"
       style={{ backgroundImage: `url(${heroImageDesktop})` }}
     >
       <div className="absolute inset-0 bg-black/60" />
 
-      <div className="relative h-full flex flex-row lg:itmes-start  xl:items-center  2xl:pb-[180px] justify-center px-4 sm:px-6 lg:px-2 lg:py-[80px]">
 
+    {/* Contenedor principal del hero */}
 
-        {/*Conteneodr izquierdo imagenes */}
-        <div className='relative w-1/2 lg:h-[600px] '>
+      <div className="relative h-full flex flex-col lg:flex-row lg:itmes-start  xl:items-center  2xl:pb-[180px] justify-center px-4 sm:px-6 lg:px-2 lg:py-[80px]">
 
-          {/*Contenedor hijo izq imagenes */}
-          <div className="relative h-full w-full flex flex-col  items-center">
+            {/*Conteneodr izquierdo imagenes */}
 
+            <div className='relative h-[400px] lg:w-1/2 lg:h-[600px] '>
 
-            <img
-              src={inoue}
-              alt="Leon"
-              className="absolute w-32 h-32 mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
-                lg:w-[500px] lg:h-[500px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-80 lg:-translate-y-[275px]"
-            />
-            <img
-              src={kyoshi}
-              alt="Leon"
-              className="absolute w-32 h-32 mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
-                lg:w-[400px] lg:h-[400px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-15 lg:-translate-y-[300px]"
-            />
-            <img
-              src={senseiManuel}
-              alt="Leon"
-              className="absolute w-32 h-32 mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
-                lg:w-[400px] lg:h-[400px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-45 lg:-translate-y-[150px]"
-            />
-            <img
-              src={rika}
-              alt="Leon"
-              className="absolute w-32 h-32 mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
-                lg:w-[400px] lg:h-[400px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-78 lg:-translate-y-1/3"
-            />
-            <img
-              src={imageLeon}
-              alt="Leon"
-              className="absolute w-32 h-32 mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
-                lg:w-[380px] lg:h-[380px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-13 lg:-translate-y-[113px]"
-            />
-            <img
-              src={german}
-              alt="Leon"
-              className="absolute w-32 h-32 mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
-                lg:w-[300px] lg:h-[300px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-40 lg:-translate-y-[33px] "
-            />
+              {/*Contenedor inquierdo hijo imagenes */}
+              <div className="relative h-full lg:h-full lg:w-full  flex flex-col  items-center">
 
-                                <div className="rain-container absolute inset-0">
-                        <div className="rain">
-                            <div className="waves"></div>
-                            <div className="particles">
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                            </div>
-                        </div>
+                  {/* Blur effect  */}
+                  <div className="absolute bottom-0 lg:bottom-[-77px]  lg:transform  lg:-translate-y-[33px] lg:ml-[50px]  h-[550px] lg:w-[750px] rounded-l rounded-r "
+                  style={{
+                    animation: "color-change 10s infinite linear",
+                    backgroundImage: "radial-gradient(closest-side, currentColor, transparent)"
+                  }}></div>
+                  {/*End Blur effect  */}
+                
+                <img
+                  src={inoue}
+                  alt="Leon"
+                  className="absolute w-[280px] h-[280px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-[140px] mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
+                    lg:w-[500px] lg:h-[500px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-80 lg:-translate-y-[275px]"
+                />
+                <img
+                  src={kyoshi}
+                  alt="Leon"
+                  className="absolute w-32 h-32 mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
+                    lg:w-[400px] lg:h-[400px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-15 lg:-translate-y-[300px]"
+                />
+                <img
+                  src={senseiManuel}
+                  alt="Leon"
+                  className="absolute w-32 h-32 mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
+                    lg:w-[400px] lg:h-[400px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-45 lg:-translate-y-[150px]"
+                />
+                <img
+                  src={rika}
+                  alt="Leon"
+                  className="absolute w-32 h-32 mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
+                    lg:w-[400px] lg:h-[400px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-78 lg:-translate-y-1/3"
+                />
+                <img
+                  src={imageLeon}
+                  alt="Leon"
+                  className="absolute w-32 h-32 mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
+                    lg:w-[380px] lg:h-[380px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-13 lg:-translate-y-[113px]"
+                />
+                <img
+                  src={german}
+                  alt="Leon"
+                  className="absolute w-32 h-32 mt-4 ml-4 sm:w-40 sm:h-40 sm:mt-6 sm:ml-6 
+                    lg:w-[300px] lg:h-[300px] lg:top-1/2 lg:left-1/2 lg:transform lg:-translate-x-40 lg:-translate-y-[33px] "
+                />
+                
+
+                <div className="rain-container absolute inset-0">
+                  <div className="rain">
+                    <div className="waves"></div>
+                    <div className="particles">
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
                     </div>
+                  </div>
+                </div>
 
-            {/* Blur effect at the bottom */}
-            <style>{`
-              @keyframes color-change {
-                0% { filter: hue-rotate(0deg); }
-                100% { filter: hue-rotate(360deg); }
-              }
-            `}</style>
-            <div className="absolute bottom-0 lg:bottom-[-77px] lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:-translate-y-[33px] lg:ml-[30px]       
-                h-[100px] lg:w-[750px] bg-[radial-gradient(closest-side,#3caffc,transparent)] rounded-l rounded-r" style={{ animation: "color-change 10s infinite linear" }}></div>
-          </div>
+                {/* Blur effect at the bottom */}
 
-        </div>
+                <div className="absolute bottom-0 lg:bottom-[-55px] lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:-translate-y-[33px] lg:ml-[50px]      
+                    h-[60px] lg:w-[630px] rounded-l rounded-r opacity-90"
+                  style={{
+                    animation: "color-change 10s infinite linear",
+                    backgroundImage: "radial-gradient(closest-side, currentColor 45%, transparent )"
+                  }}></div>
 
-        {/*Contenedor derecho imagenes */}
-        <div className="w-1/2 text-left text-white max-w-lg  flex justify-center mx-auto lg:ml-12  ">
 
-          {/*Contenedor  titulos */}
-          <div className="bg-transparent backdrop-blur-xs p-8 py-8  rounded-lg">
 
-            <h1 className="text-5xl lg:text-6xl font-bold lg:text-leading-tight! ">
+
+              </div>
+            </div>
+
+            {/*Contenedor derecho imagenes */}
+
+            <div className="lg:w-1/2 text-left text-white max-w-lg   flex justify-center mx-auto lg:ml-12 ">
+
+              {/*Contenedor  titulos */}
+              <div className="bg-transparent backdrop-blur-xs p-8 py-8  border-[1px] hover:backdrop-blur-lg border-current  rounded-lg shadow-xl/50 transition-[backdrop-filter] duration-500" 
+              style={{animation: " color-change 10s infinite linear"}}>
+
+            <h1 className="text-5xl lg:text-6xl font-bold lg:text-leading-tight">
               TU CUERPO ES
               <br />
-              <span className='text-[#51b0f0]'>TU TEMPLO</span>
+              <span className="text-white">TU TEMPLO</span>
             </h1>
-            <p className="mt-4 text-lg">
-              "El dolor que sientes hoy es la fuerza que sentirás mañana. Cada repetición, cada gota de sudor, te acerca a la mejor versión de ti mismo. ¡No te rindas!"
-            </p>
-            <div className="mt-8">
-              <a
-                href="#"
-                className="inline-block bg-[#3caffc] hover:bg-[#032c85] text-white font-bold py-3 px-8 rounded-lg text-lg"
-              >
-                Empieza Hoy
-              </a>
+                <p className="mt-4 text-lg text-white">
+                  "El dolor que sientes hoy es la fuerza que sentirás mañana. Cada repetición, cada gota de sudor, te acerca a la mejor versión de ti mismo. ¡No te rindas!"
+                </p>
+
+                {/*Boton */}
+                <div className="mt-8" >
+                  <a
+                  href="#"
+                  className="inline-block bg-transparent backdrop-blur-sm  border hover-border-white   hover:bg-color-change  hover:text-white font-regular py-3 px-8 rounded-lg text-lg"
+                  style={{ animation: "border-color-change  10s infinite linear" }}
+                >
+                  Empieza Hoy
+                </a>
+                </div>
+                {/*end Boton */}
+
+
+                <div className="mt-8 flex justify-end space-x-4">
+                  <a href="#" className="text-white hover:text-indigo-400">
+                    <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M22.46 6c-.8.36-1.65.6-2.54.7.9-.54 1.6-1.4 1.92-2.44-.84.5-1.78.86-2.78 1.06C18.26 4.3 17.1 4 15.82 4c-2.44 0-4.42 1.98-4.42 4.42 0 .34.04.68.1.1L7.1 9.42C6.7 7.9 5.8 6.63 4.58 5.82c-.34.58-.53 1.25-.53 1.96 0 1.5.76 2.84 1.94 3.62-.7-.02-1.36-.22-1.94-.54v.06c0 2.1 1.5 3.85 3.5 4.25-.36.1-.74.14-1.14.14-.28 0-.55-.02-.82-.08.56 1.72 2.18 2.97 4.1 3-1.48 1.16-3.36 1.86-5.4 1.86-.35 0-.7-.02-1.04-.06 1.92 1.24 4.2 1.96 6.68 1.96 8.02 0 12.4-6.64 12.4-12.4v-.56c.84-.6 1.58-1.36 2.16-2.24z" />
+                    </svg>
+                  </a>
+                  <a href="#" className="text-white hover:text-indigo-400">
+                    <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                      <path fillRule="evenodd" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm3.2 6.78h-1.34c-.5 0-.6.24-.6.6v.94h1.86l-.24 1.86h-1.62v4.78h-1.94v-4.78H9v-1.86h1.32v-.7c0-1.3.8-2.02 1.96-2.02h1.6v1.88z" clipRule="evenodd" />
+                    </svg>
+                  </a>
+                  <a href="#" className="text-white hover:text-indigo-400">
+                    <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                      <path fillRule="evenodd" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.6 5.8c.2.1.3.2.4.4.1.2.2.4.2.6v6.4c0 .4-.1.7-.4.9-.2.2-.5.3-.8.3h-1.2c-.3 0-.6-.1-.8-.3-.2-.2-.4-.5-.4-.9V9.4c0-.4.1-.7.4-.9.2-.2.5-.3.8-.3h1.2c.3 0 .6.1.8.3zm-3.6 0c.2.1.3.2.4.4.1.2.2.4.2.6v6.4c0 .4-.1.7-.4.9-.2.2-.5.3-.8.3h-1.2c-.3 0-.6-.1-.8-.3-.2-.2-.4-.5-.4-.9V9.4c0-.4.1-.7.4-.9.2-.2.5-.3.8-.3h1.2c.3 0 .6.1.8.3zm-3.6 0c.2.1.3.2.4.4.1.2.2.4.2.6v6.4c0 .4-.1.7-.4.9-.2.2-.5.3-.8.3H8c-.3 0-.6-.1-.8-.3-.2-.2-.4-.5-.4-.9V9.4c0-.4.1-.7.4-.9.2-.2.5-.3.8-.3h1.2c.3 0 .6.1.8.3z" clipRule="evenodd" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
             </div>
-            <div className="mt-8 flex justify-end space-x-4">
-              <a href="#" className="text-white hover:text-indigo-400">
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M22.46 6c-.8.36-1.65.6-2.54.7.9-.54 1.6-1.4 1.92-2.44-.84.5-1.78.86-2.78 1.06C18.26 4.3 17.1 4 15.82 4c-2.44 0-4.42 1.98-4.42 4.42 0 .34.04.68.1.1L7.1 9.42C6.7 7.9 5.8 6.63 4.58 5.82c-.34.58-.53 1.25-.53 1.96 0 1.5.76 2.84 1.94 3.62-.7-.02-1.36-.22-1.94-.54v.06c0 2.1 1.5 3.85 3.5 4.25-.36.1-.74.14-1.14.14-.28 0-.55-.02-.82-.08.56 1.72 2.18 2.97 4.1 3-1.48 1.16-3.36 1.86-5.4 1.86-.35 0-.7-.02-1.04-.06 1.92 1.24 4.2 1.96 6.68 1.96 8.02 0 12.4-6.64 12.4-12.4v-.56c.84-.6 1.58-1.36 2.16-2.24z" />
-                </svg>
-              </a>
-              <a href="#" className="text-white hover:text-indigo-400">
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                  <path fillRule="evenodd" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm3.2 6.78h-1.34c-.5 0-.6.24-.6.6v.94h1.86l-.24 1.86h-1.62v4.78h-1.94v-4.78H9v-1.86h1.32v-.7c0-1.3.8-2.02 1.96-2.02h1.6v1.88z" clipRule="evenodd" />
-                </svg>
-              </a>
-              <a href="#" className="text-white hover:text-indigo-400">
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                  <path fillRule="evenodd" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.6 5.8c.2.1.3.2.4.4.1.2.2.4.2.6v6.4c0 .4-.1.7-.4.9-.2.2-.5.3-.8.3h-1.2c-.3 0-.6-.1-.8-.3-.2-.2-.4-.5-.4-.9V9.4c0-.4.1-.7.4-.9.2-.2.5-.3.8-.3h1.2c.3 0 .6.1.8.3zm-3.6 0c.2.1.3.2.4.4.1.2.2.4.2.6v6.4c0 .4-.1.7-.4.9-.2.2-.5.3-.8.3h-1.2c-.3 0-.6-.1-.8-.3-.2-.2-.4-.5-.4-.9V9.4c0-.4.1-.7.4-.9.2-.2.5-.3.8-.3h1.2c.3 0 .6.1.8.3zm-3.6 0c.2.1.3.2.4.4.1.2.2.4.2.6v6.4c0 .4-.1.7-.4.9-.2.2-.5.3-.8.3H8c-.3 0-.6-.1-.8-.3-.2-.2-.4-.5-.4-.9V9.4c0-.4.1-.7.4-.9.2-.2.5-.3.8-.3h1.2c.3 0 .6.1.8.3z" clipRule="evenodd" />
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
+
       </div>
+
+
     </div>
   );
 };
